c98ca0205335cef5df421e140a4a111823bce068
kane <2205485120@qq.com>
2017-08-31 18:44:12 +0800

    admin can upload file to create a batch user on admin page

--- out/production/resources/static/js/admin-ajax.js
+++ out/production/resources/static/js/admin-ajax.js
@@ -33,6 +33,17 @@ $(document).ready(function () {
     });
 
     $('#batch_user').click(function () {
-
+        var form_data = $('#batch_user_form');
+        $.ajaxPlanH({
+            url: '/user/batch',
+            data: new FormData(form_data[0]),
+            cache: false,
+            success: function () {
+                alert('成功');
+            },
+            error: function () {
+                alert('失败');
+            }
+        });
     })
 });
