29de1dd67c4d2e411efd6f285fa1b99e3d77770e
kane <2205485120@qq.com>
2017-09-27 21:58:32 +0800

    add error page

--- src/main/java/top/kanetah/planH/config/MvcConfig.java
+++ src/main/java/top/kanetah/planH/config/MvcConfig.java
@@ -2,10 +2,11 @@ package top.kanetah.planH.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.*;
 import top.kanetah.planH.interceptor.PlanHLoginInterceptor;
 
@@ -17,8 +18,12 @@ public class MvcConfig implements WebMvcConfigurer {
     private final PlanHLoginInterceptor loginInterceptor;
 
     @Autowired
-    public MvcConfig(PlanHLoginInterceptor loginInterceptor) {
+    public MvcConfig(
+            PlanHLoginInterceptor loginInterceptor,
+            TomcatServletWebServerFactory tomcatServletWebServerFactory
+    ) {
         this.loginInterceptor = loginInterceptor;
+        tomcatServletWebServerFactory.addErrorPages(new ErrorPage("/error"));
     }
 
     @Override
@@ -34,6 +39,7 @@ public class MvcConfig implements WebMvcConfigurer {
     ) {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/admin/").setViewName("admin_x");
+        registry.addViewController("/error").setViewName("error");
     }
 
     @Override
@@ -55,4 +61,25 @@ public class MvcConfig implements WebMvcConfigurer {
         factory.setMaxRequestSize("1056MB");
         return factory.createMultipartConfig();
     }
+
+//    @Bean
+//    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
+//        return new EmbeddedServletContainerCustomizer() {
+//            @Override
+//            public void customize(ConfigurableEmbeddedServletContainer container) {
+//                container.setSessionTimeout(1, TimeUnit.MINUTES);
+//            }
+//        };
+//    }
+//    @Bean
+//    public ConfigurableEmbeddedServletContainer containerCustomizer() {
+//
+//        return (container -> {
+//            ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+//            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+//            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+//
+//            container.addErrorPages(error401Page, error404Page, error500Page);
+//        });
+//    }
 }
